b01bd6668454a58ef951ecca04a2745e1e0acd51
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-01 07:49:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/super-news/trunk@317341 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/functions/translations.inc.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit ("Do not access this file directly.");
+/*
+Mangles internal translations.
+Attach to: add_filter("gettext");
+*/
+if (!function_exists ("ws_plugin__super_news_translation_mangler"))
+	{
+		function ws_plugin__super_news_translation_mangler ($translated = FALSE, $original = FALSE, $domain = FALSE)
+			{
+				static $is_admin_media_upload; /* Optimizes this routine. */
+				/**/
+				if (!isset ($is_admin_media_upload) || $is_admin_media_upload)
+					{
+						if ($is_admin_media_upload || (is_admin () && preg_match ("/\/(async-upload|media-upload)\.php/", $_SERVER["REQUEST_URI"])))
+							{
+								$is_admin_media_upload = true;
+								/**/
+								if ($translated === "Insert into Post")
+									{
+										$translated = apply_filters ("ws_plugin__super_news_translation_mangler", "Insert", get_defined_vars ());
+									}
+							}
+						else
+							{
+								$is_admin_media_upload = false;
+							}
+					}
+				/**/
+				return $translated;
+			}
+	}
+?>
