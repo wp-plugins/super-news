97c0ca769e42f28dbac6c092be51a3c11117fcbe
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-11 04:06:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/super-news/tags/1.2.2@371402 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/default-styles.inc.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit ("Do not access this file directly.");
+/**/
+if (!class_exists ("c_ws_plugin__super_news_default_styles"))
+	{
+		class c_ws_plugin__super_news_default_styles
+			{
+				/*
+				Function for adding styles to the header.
+				Attach to: add_action("wp_print_styles");
+				*/
+				public static function default_styles ()
+					{
+						do_action ("ws_plugin__super_news_before_default_styles", get_defined_vars ());
+						/**/
+						if (!is_admin () && $GLOBALS["WS_PLUGIN__"]["super_news"]["o"]["enqueue_styles"])
+							{
+								wp_enqueue_style ("ws-plugin--super-news", site_url ("/?ws_plugin__super_news_css=1"), array (), c_ws_plugin__super_news_utilities::ver_checksum (), "all");
+								/**/
+								do_action ("ws_plugin__super_news_during_default_styles", get_defined_vars ());
+							}
+						/**/
+						do_action ("ws_plugin__super_news_after_default_styles", get_defined_vars ());
+						/**/
+						return; /* Return for uniformity. */
+					}
+				/*
+				Function for adding styles to the search page.
+				Attach to: add_action("admin_print_styles");
+				*/
+				public static function search_styles ()
+					{
+						do_action ("ws_plugin__super_news_before_search_styles", get_defined_vars ());
+						/**/
+						if (c_ws_plugin__super_news_utils_conds::is_blog_admin () && $_GET["page"] === "ws-plugin--super-news-search")
+							{
+								wp_enqueue_style ("ws-plugin--super-news", site_url ("/?ws_plugin__super_news_css=1"), array (), c_ws_plugin__super_news_utilities::ver_checksum (), "all");
+								/**/
+								do_action ("ws_plugin__super_news_during_search_styles", get_defined_vars ());
+							}
+						/**/
+						do_action ("ws_plugin__super_news_after_search_styles", get_defined_vars ());
+						/**/
+						return; /* Return for uniformity. */
+					}
+			}
+	}
+?>
